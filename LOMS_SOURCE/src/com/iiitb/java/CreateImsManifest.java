8fc61515cd5446aa353722b0094409fba6810b0d
Ashrith Kulai <ashrithkulai@gmail.com>
2012-03-31 12:04:25 +0530

    manifest beautifications :P

--- LOMS_SOURCE/src/com/iiitb/java/CreateImsManifest.java
+++ LOMS_SOURCE/src/com/iiitb/java/CreateImsManifest.java
@@ -16,6 +16,7 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import java.util.Random;
 
 
 public class CreateImsManifest {
@@ -24,7 +25,9 @@ public class CreateImsManifest {
    public void CreateExportImsManifest(String rootDir,String packageName, ArrayList<ManifestData> imsFileInfo) {
 
         try {
-            
+            int min=100000, max=1000000;
+            Random randObj = new Random();
+            int manifestId=(randObj.nextInt(max - min) + min);            
             //System.out.println("Manifest Code: Root Dir = "+rootDir);
 
             DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
@@ -39,7 +42,7 @@ public class CreateImsManifest {
             rootElement.setAttribute("xmlns:imsmd","http://www.imsglobal.org/xsd/imsmd_rootv1p2p1");
             rootElement.setAttribute("xmlns:xsi","http://www.w3.org/2001/XMLSchema-instance");
             rootElement.setAttribute("xmlns:adlcp","http://www.adlnet.org/xsd/adlcp_rootv1p2");
-            rootElement.setAttribute("identifier","MANIFEST_selfpack02"); 
+            rootElement.setAttribute("identifier","MANIFEST_"+manifestId); 
             rootElement.setAttribute("xsi:schemaLocation",
                     "http://www.imsproject.org/xsd/imscp_rootv1p1p2 imscp_rootv1p1p2.xsd "       
                     + "http://www.imsglobal.org/xsd/imsmd_rootv1p2p1 imsmd_rootv1p2p1.xsd "
@@ -48,11 +51,11 @@ public class CreateImsManifest {
 
             Element organizations = doc.createElement("organizations");
             rootElement.appendChild(organizations);
-            organizations.setAttribute("default", "org-1");
+            organizations.setAttribute("default", "ORG-1");
 
             Element organization = doc.createElement("organization");
             organizations.appendChild(organization);
-            organization.setAttribute("identifier", "org-1");
+            organization.setAttribute("identifier", "ORG-1");
             organization.setAttribute("structure", "hierarchical");
 
             Element title = doc.createElement("title");
@@ -61,17 +64,12 @@ public class CreateImsManifest {
             
             int item_count = 1;
             for(ManifestData md: imsFileInfo){
-//                String fName=file.getName();
-//                StringTokenizer st1 = new StringTokenizer(fName, ".");
-//
-//                String temp = (String) st1.nextElement();
-                //System.out.println("iteration:"+item_count); 
-                //System.out.println(temp);
+
                 Element item = doc.createElement("item");
                 organization.appendChild(item);
 
-                item.setAttribute("identifier", "item_"+item_count);
-                item.setAttribute("identifierref", "res_"+item_count++);
+                item.setAttribute("identifier", "ITEM_"+item_count);
+                item.setAttribute("identifierref", "RES_"+item_count++);
                 item.setAttribute("isvisible", "true");
 
                 Element title1 = doc.createElement("title");
@@ -86,28 +84,9 @@ public class CreateImsManifest {
             rootElement.appendChild(resources);
             item_count = 1;        
             for(ManifestData md: imsFileInfo){
-//                String name = file.getName();
-//                System.out.println(name);
-//                System.out.println(file.getAbsoluteFile());
-//                System.out.println(file.getAbsolutePath());
-//                try {
-//                    System.out.println(file.getCanonicalFile().toString());
-//                    System.out.println(file.getCanonicalPath());
-//                } catch (IOException ex) {
-//                    Logger.getLogger(CreateImsManifest.class.getName()).log(Level.SEVERE, null, ex);
-//                }
-//                
-//                 System.out.println(file.getParent());
-//                 System.out.println(file.getParentFile());
-//                  System.out.println(file.getPath());
-                
-                
-                
-                
-                
                 Element resource1 = doc.createElement("resource");
                 resources.appendChild(resource1);
-                resource1.setAttribute("identifier",  "res_"+item_count++);
+                resource1.setAttribute("identifier",  "RES_"+item_count++);
                 resource1.setAttribute("href",md.getHref());
                 resource1.setAttribute("adlcp:scormtype", "sco");
                 resource1.setAttribute("type", "webcontent");
